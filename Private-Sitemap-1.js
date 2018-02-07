8a02db8ee78c6e4b7e5eaf6166ec3cd88db5a226
CraftingBag <35647607+CraftingBag@users.noreply.github.com>
2018-02-07 14:51:04 +0700

    Private-Sitemap-1.js
    This Private

---
+++ Private-Sitemap-1.js
@@ -0,0 +1,31 @@
+
+var numposts = 100;
+var standardstyling = true;
+
+
+function startpost(json){
+  for (var i = 0; i < numposts; i++){
+    var entry = json.feed.entry[i];
+    var posttitle = entry.title.$t;
+    var posturl;
+    
+    if (i == json.feed.entry.length) break;
+    
+    for (var k = 0; k < entry.link.length; k++){
+      if (entry.link[k].rel == 'alternate'){
+        posturl = entry.link[k].href;break;
+      }
+    }
+        
+    posttitle = posttitle.link(posturl);
+    if (standardstyling) document.write('<li>');
+    
+    document.write(posttitle);
+  }
+  
+  if (standardstyling) document.write('</li>');
+}
+
+function finished(){
+  document.write('<br /><br /><a href="http://www.craftingbag.ga/2018/01/cara-membuat-sitemap-otomatis-diblogger.html" style="font-size: 10 px; text-decoration:none; color: #616469;" target="_blank" title="Cara Buat Daftar Isi di Blogspot">Sitemap</a>');
+}
