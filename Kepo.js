6f6b8aa211f03b567299bbb256bb0ad3888f634f
CraftingBag <35647607+CraftingBag@users.noreply.github.com>
2018-05-03 16:46:36 +0700

    Create Kepo.js

---
+++ Kepo.js
@@ -0,0 +1,26 @@
+<div id='oopss'>
+<style>
+/* CSS Custom Error Page */
+#oopss {background-position:center;background: url(&#39;https://1.bp.blogspot.com/-97s4Bqkc2KA/V4TnVL0dAqI/AAAAAAAAAQ0/lWttAbi9nDky6GMusDgs0SD48aizrxOJACKgB/s1600/Anime-Wallpaper-Images-J5P%255B1%255D.png&#39;)no-repeat center top fixed;-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;text-align:center;margin-bottom:50px;font-weight:400;font-size:45px;font-family:&#39;Roboto&#39;,Arial,sans-serif;position:fixed;width:100%;height:100%;line-height:1.5em;z-index:9999;}
+#error-text {top:30%;position:relative;font-size:50px;color:#fff;text-shadow: 0 4px 0 #ddd,0 8px 8px #aaa;}
+#error-text a{color:#888;}
+#error-text p {margin:0!important;}
+#error-text span {color:#e47253;font-size:100px;text-shadow:0 4px 0 #a64126,0 8px 8px #aaa;letter-spacing:20px;transition:all 0.3s ease-in-out;}
+#error-text span:hover {letter-spacing:30px;}
+#error-text a {color:#e47253;}
+#error-text a:hover {color:#394347;letter-spacing:-10px;}
+#wrapper, .ct-wrapper, .header-wrapper, #header, #post-wrapper, #outer-wrapper, #footer-wrapper, #sidebar-wrapper, #credit, #social_networks {display:none;}
+/* CSS Error Page Responsive */
+@media only screen and (max-width:640px){
+#error-text {color:#888;font-size:30px;text-shadow:none;}
+#error-text span {font-size:60px;text-shadow:none;letter-spacing:0;}
+#error-text span:hover {letter-spacing:5px;}
+#error-text a:hover {color:#394347;letter-spacing:5px;}}
+</style>
+<div id='error-text'>
+<span>WARNING!!!</span>
+<p>Kenapa kamu datang ke halaman ini?</p>
+  <p>Pasti kamu kepo sama aku yah?</p>
+  <p>Lihat profilku klik disamping <a href='http://facebook.com/syahrul.falah.999'><i CLASS='fa fa-arrow-circle-right'/></a></p>
+</div>
+</div>
